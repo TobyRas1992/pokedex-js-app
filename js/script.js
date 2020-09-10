8a8877a90c6cb67009e2409a374b58e745379a9d
TobyRas1992 <tobiasrasmussen@outlook.com>
2020-09-10 13:37:55 +0200

    A few more pokemon added to the list

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let pokemonList = [
   {name: 'Bulbasaur', type: ['grass', 'poison'], height: 2},
+  {name: 'Charizard', type: ['fire', 'flying'], height: 5.07},
+  {name: 'Jigglypuff', type: ['normal', 'fairy'], height: 1.08},
+  {name: 'Stunky', type: ['poison', 'dark'], height: 1.04},
   {name: 'Slowpoke', type: ['water', 'psychic'], height: 3},
   {name: 'Onix', type: ['rock', 'ground'], height: 28},
-
+  {name: 'Infernape', type: ['fire', 'fighting'], height: 3.11}
 ];
