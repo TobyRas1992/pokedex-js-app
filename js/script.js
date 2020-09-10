096f4ceeb94a89c84f2f1ed309a1b18a327030eb
TobyRas1992 <tobiasrasmussen@outlook.com>
2020-09-10 13:31:44 +0200

    Pokemon list created

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
-let favoriteFood = 'Pizza';
-alert('Hello world');
-document.write(favoriteFood);
+let pokemonList = [
+  {name: 'Bulbasaur', type: ['grass', 'poison'], height: 2},
+  {name: 'Slowpoke', type: ['water', 'psychic'], height: 3},
+  {name: 'Onix', type: ['rock', 'ground'], height: 28},
+
+];
