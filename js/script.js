a76baf3c6b354f7de03f551dee1823638497844d
TobyRas1992 <tobiasrasmussen@outlook.com>
2020-09-12 09:59:04 +0200

    Fixed a naming error in the link to my stylesheet in my HTML + added a few more vairables in my JS script

--- js/script.js
+++ js/script.js
@@ -7,11 +7,15 @@ let pokemonList = [
   {name: 'Onix', type: ['rock', 'ground'], height: 28},
   {name: 'Infernape', type: ['fire', 'fighting'], height: 3.11}
 ];
+let introText1 = ' (height: ';
+let introText2= ')';
+let specialText = ') - Wow, that\'s a big Pokemon!'
+let space = ' '
 
 for (i=0; pokemonList.length; i++){
   if (pokemonList[i].height > 20) {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s a big Pokemon!');
+    document.write(pokemonList[i].name + introText1 + pokemonList[i].height + specialText + space);
   } else {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + introText2 + space);
   }
 }
